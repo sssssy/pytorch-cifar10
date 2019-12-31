2b3da973d76286105bfc14e5117c7178a46fed61
sssssy <suosuosuosuosuoyuan@outlook.com>
2019-12-31 10:31:32 +0000

    checkpoint 12311028

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torch.optim as optim
 import time
@@ -21,26 +22,63 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
  
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.relu = nn.ReLU()
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, 10)
+ 
+#     def forward(self, x):
+#         x = self.pool(self.relu(self.conv1(x)))
+#         x = self.pool(self.relu(self.conv2(x)))
+#         x = x.view(-1, 16 * 5 * 5)
+#         x = self.relu(self.fc1(x))
+#         x = self.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
+ 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(3, 64, 3, padding = 1)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding = 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding = 1)
+        self.conv4 = nn.Conv2d(128, 128, 3, padding = 1)
+        self.conv5 = nn.Conv2d(128, 256, 3, padding = 1)
+        self.conv6 = nn.Conv2d(256, 256, 3, padding = 1)
+        self.maxpool = nn.MaxPool2d(2, 2)
+        self.avgpool = nn.AvgPool2d(2, 2)
+        self.globalavgpool = nn.AvgPool2d(8, 8)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.dropout50 = nn.Dropout(0.5)
+        self.dropout10 = nn.Dropout(0.1)
+        self.fc = nn.Linear(256, 10)
  
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.bn1(F.relu(self.conv1(x)))
+        x = self.bn1(F.relu(self.conv2(x)))
+        x = self.maxpool(x)
+        x = self.dropout10(x)
+        x = self.bn2(F.relu(self.conv3(x)))
+        x = self.bn2(F.relu(self.conv4(x)))
+        x = self.avgpool(x)
+        x = self.dropout10(x)
+        x = self.bn3(F.relu(self.conv5(x)))
+        x = self.bn3(F.relu(self.conv6(x)))
+        x = self.globalavgpool(x)
+        x = self.dropout50(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
         return x
- 
+
 net = Net()
  
 criterion = nn.CrossEntropyLoss()
