76e931db73feef31623b47bc3985d2bd3ebc1375
sssssy <suosuosuosuosuoyuan@outlook.com>
2020-01-02 13:20:57 +0000

    update main.py, model.py

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ from config import DefaultConfig
 
 opt = DefaultConfig()
  
-transform_train = transforms.Compose(
+transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
-    [transforms.ToTensor(),
+    transforms.ToTensor(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
 transform_test = transforms.Compose(
