eab2c12b927fb6f756cb87517f2fd18a89c78f8b
sssssy <suosuosuosuosuoyuan@outlook.com>
2020-01-02 11:01:35 +0000

    update config.py, debug model.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class DefaultConfig(object):
 	batch_size = 64
 	use_gpu = True
 	num_workers = 4
-	print_freq = 100# 每几个batch打印一次信息
+	print_freq = 500# 每几个batch打印一次信息
 	val_freq = 400# 每几个batch调用一次val，以供调试
 
 	max_epoch = 10
@@ -20,17 +20,10 @@ class DefaultConfig(object):
 	weight_decay = 1e-4
 
 	debug_file = './debug'
-
-	def parse(self, kwargs):
-		'''
-		根据字典kwargs更新参数
-		'''
-		for k, v in kwargs.items():
-			if not hasattr(self, k):
-				print("\n!!!Warning: opt has no attribute %s!!!\n" %k)
-			setattr(self, k, v)
-
+	
+	def __init__(self):
 		print('User config: ')
 		for k, v in self.__class__.__dict__.items():
 			if not k.startswith('__'):
 				print(k, getattr(self, k))
+				
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 class BasicNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(BasicNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -23,7 +23,7 @@ class BasicNet(nn.Module):
 
 class FullConvNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(FullConvNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, 3, padding = 1)
         self.conv2 = nn.Conv2d(64, 64, 3, padding = 1)
         self.conv3 = nn.Conv2d(64, 128, 3, padding = 1)
