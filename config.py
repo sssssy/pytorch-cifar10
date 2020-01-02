9fc9a1c7bb1f6b8d9709c531840fe209dd0efa8c
sssssy <suosuosuosuosuoyuan@outlook.com>
2020-01-02 16:02:15 +0800

    update config.py & main.py for this task

--- config.py
+++ config.py
@@ -2,24 +2,22 @@
 from __future__ import print_function
 
 class DefaultConfig(object):
-	model = 'CaviModel'
 
-	root = 'E:/Pytorch Projects/Caricature recognition/IIIT-CFW/IIIT-CFW1.0/'
-	load_model_path = None#'/data2/fanjiahui/CaricatureRecognition/test/checkpoints/'
+	root = '/fanjiahui/repositories/pytorch-cifar10'
+	load_model_path = None
 	# 需要加载模型或者测试时才要给这个参数
 
 	input_channels = 3
 	batch_size = 64
 	use_gpu = True
 	num_workers = 4
-	print_freq = 20# 每几个batch打印一次信息
+	print_freq = 100# 每几个batch打印一次信息
 	val_freq = 400# 每几个batch调用一次val，以供调试
 
 	max_epoch = 10
 	lr = 0.001
 	lr_decay = 0.95
 	weight_decay = 1e-4
-	threshold = 0.5# softmax输出的判别阈值
 
 	debug_file = './debug'
 
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torch.optim as optim
 import time
+
+from model import *
+from config import DefaultConfig
+
+######################################
+# Initializing
+######################################
  
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -22,81 +29,36 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
  
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# class Net(nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 5)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(6, 16, 5)
-#         self.relu = nn.ReLU()
-#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-#         self.fc2 = nn.Linear(120, 84)
-#         self.fc3 = nn.Linear(84, 10)
- 
-#     def forward(self, x):
-#         x = self.pool(self.relu(self.conv1(x)))
-#         x = self.pool(self.relu(self.conv2(x)))
-#         x = x.view(-1, 16 * 5 * 5)
-#         x = self.relu(self.fc1(x))
-#         x = self.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
- 
+opt = DefaultConfig()
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 3, padding = 1)
-        self.conv2 = nn.Conv2d(64, 64, 3, padding = 1)
-        self.conv3 = nn.Conv2d(64, 128, 3, padding = 1)
-        self.conv4 = nn.Conv2d(128, 128, 3, padding = 1)
-        self.conv5 = nn.Conv2d(128, 256, 3, padding = 1)
-        self.conv6 = nn.Conv2d(256, 256, 3, padding = 1)
-        self.maxpool = nn.MaxPool2d(2, 2)
-        self.avgpool = nn.AvgPool2d(2, 2)
-        self.globalavgpool = nn.AvgPool2d(8, 8)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.dropout50 = nn.Dropout(0.5)
-        self.dropout10 = nn.Dropout(0.1)
-        self.fc = nn.Linear(256, 10)
- 
-    def forward(self, x):
-        x = self.bn1(F.relu(self.conv1(x)))
-        x = self.bn1(F.relu(self.conv2(x)))
-        x = self.maxpool(x)
-        x = self.dropout10(x)
-        x = self.bn2(F.relu(self.conv3(x)))
-        x = self.bn2(F.relu(self.conv4(x)))
-        x = self.avgpool(x)
-        x = self.dropout10(x)
-        x = self.bn3(F.relu(self.conv5(x)))
-        x = self.bn3(F.relu(self.conv6(x)))
-        x = self.globalavgpool(x)
-        x = self.dropout50(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
-
+'''
+ Remember to update this!
+'''
 net = Net()
- 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+if opt.use_gpu = True:
+    net.cuda()
 
-net.cuda()
+########################################
+# Training
+########################################
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=opt.lr)
 
 for epoch in range(10):
  
     running_loss = 0.
-    batch_size = 100
+
+    batch_size = opt.batch_size
     
     for i, data in enumerate(
             torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                        shuffle=True, num_workers=2), 0):
+                                        shuffle=True, num_workers=opt.num_workers), 0):
         
         inputs, labels = data
-        inputs, labels = inputs.cuda(), labels.cuda()
+        if opt.use_gpu = True:
+            inputs, labels = inputs.cuda(), labels.cuda()
         
         optimizer.zero_grad()
         
@@ -105,10 +67,14 @@ for epoch in range(10):
         loss.backward()
         optimizer.step()
         
-        if i % 100 == 0:
+        if i % opt.print_freq == 0:
 	        print('[%d, %5d] loss: %.4f' %(epoch + 1, i, loss.item()))
  
 print('Finished Training')
+
+########################################
+# save the model
+########################################
 
 prefix = './checkpoints/'
 current_time = time.strftime("%m-%d_%H%M", time.localtime())
@@ -121,7 +87,8 @@ total = 0
 with torch.no_grad():
     for data in testloader:
         images, labels = data
-        images, labels = images.cuda(), labels.cuda()
+        if opt.use_gpu = True:
+            images, labels = images.cuda(), labels.cuda()
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
@@ -135,7 +102,8 @@ class_total = list(0. for i in range(10))
 with torch.no_grad():
     for data in testloader:
         images, labels = data
-        images, labels = images.cuda(), labels.cuda()
+        if opt.use_gpu = True:
+            images, labels = images.cuda(), labels.cuda()
         outputs = net(images)
         _, predicted = torch.max(outputs, 1)
         c = (predicted == labels).squeeze()
